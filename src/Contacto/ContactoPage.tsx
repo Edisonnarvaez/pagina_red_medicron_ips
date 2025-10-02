be7b85cd01be68131285e86bd6023ada268c763e
Edisonnarvaez <edison.narvaez@campusucc.edu.co>
2025-10-02 15:12:27 -0500

    se actualizan telefonos

--- src/Contacto/ContactoPage.tsx
+++ src/Contacto/ContactoPage.tsx
@@ -237,7 +237,7 @@ const Contacto: React.FC = () => {
                                                 <p className="text-gray-600 font-medium">+57 (316) 090-2783</p>
                                                 <p className="text-gray-600 font-medium">+57 (318) 3807-4300</p>
                                                 <p className="text-sm text-gray-500">Lineas para citas de FOMAG</p>
-                                                <p className="text-gray-600 font-medium">+57 (602) 738-2377</p>
+                                                <p className="text-gray-600 font-medium">(602) 738-2377</p>
                                                 <p className="text-sm text-gray-500">Lun - Vie: 7:00 AM - 3:30 PM</p>
                                                 <p className="text-gray-600">Hospital Túquerres: +57 (321) 666-0990</p>
                                                 <p className="text-sm text-gray-500">Lun - Vie: 7:00 AM - 5:00 PM</p>
